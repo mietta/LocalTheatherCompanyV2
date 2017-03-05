41e10238d6e4824b0d2d977325c1e247561012f5
Mie Tanaka <mietta25@gmail.com>
2017-03-05 09:59:36 +0000

    Add project files.

---
+++ MieTanakaLocalTheaterCompanyV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MieTanakaLocalTheaterCompanyV2.Startup))]
+namespace MieTanakaLocalTheaterCompanyV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
